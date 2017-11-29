bf635b937b3eb05654f15b904a3ec6ada834e0e8
Linus <linus@leute.server.de>
2017-11-29 19:07:57 +0100

    Simplify inline conditional rendering

--- src/renderer/components/loader/index.js
+++ src/renderer/components/loader/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Loader(props) {
   return (
-    <div className={ 'loader' + (props.visible ? ' visible' : '') }>
+    <div className={props.visible ? 'loader visible' : 'loader'}>
       <div className="bounce1"></div>
       <div className="bounce2"></div>
       <div className="bounce3"></div>
