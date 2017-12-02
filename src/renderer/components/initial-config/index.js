6d66934ce7850c4da857ad1831a46e92608a4af3
Linus <linus@leute.server.de>
2017-12-02 01:23:02 +0100

    Add (failing) code for a simple first-run-configuration

---
+++ src/renderer/components/initial-config/index.js
@@ -0,0 +1,59 @@
+import Config from 'electron-config';
+import React from 'react';
+
+const config = new Config();
+
+
+export class InitialConfig extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pythonBinary: '',
+      spotdlScript: '',
+      submitDisabled: true,
+      formProcessed: false
+    };
+
+    this.handleChangePython = this.handleChangePython.bind(this);
+    this.handleChangeSpotdl = this.handleChangeSpotdl.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangePython(event) {
+    let pythonBinary = event.target.value;
+    let submitDisabled = !(pythonBinary && this.state.spotdlScript);
+    this.setState({
+      pythonBinary: pythonBinary,
+      submitDisabled: submitDisabled
+    });
+  }
+
+  handleChangeSpotdl(event) {
+    let spotdlScript = event.target.value;
+    let submitDisabled = !(spotdlScript && this.state.pythonBinary);
+    this.setState({
+      spotdlScript: spotdlScript,
+      submitDisabled: submitDisabled
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({formProcessed: true});
+    config.set('python_binary', this.state.pythonBinary);
+    config.set('spotdl_script', this.state.spotdlScript);
+    event.preventDefault();
+  }
+
+  render() {
+    if (this.state.formProcessed || (config.get('python_binary') && config.get('spotdl_script'))) return null;
+    return (
+      <div id="initial-config">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.pythonBinary} onChange={this.handleChangePython} placeholder="Path to Python executable" className="mb-40 block full-width" />
+          <input type="text" value={this.state.spotdlScript} onChange={this.handleChangeSpotdl} placeholder="Path top spotdl.py script" className="mb-40 block full-width" />
+          <input type="submit" value="Save" disabled={this.state.submitDisabled} className="bg-green fg-light block centered" />
+        </form>
+      </div>
+    );
+  }
+}
--- src/renderer/components/main/index.js
+++ src/renderer/components/main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { InitialConfig } from '../initial-config';
 import { PlaylistInput } from '../playlist-input';
 
 export class Main extends React.Component {
   render() {
     return (
       <div className="container">
+        <InitialConfig />
         <PlaylistInput />
       </div>
     );
